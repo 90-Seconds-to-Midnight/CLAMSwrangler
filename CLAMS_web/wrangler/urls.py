1f6395eec50437097beccd7a845414703ac42167
sclayton33 <stu3cla@pm.me>
2023-11-13 14:51:13 -0500

    Remove web specific code, forked to separate repo.

--- CLAMS_web/wrangler/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import (
-    homepage_view,
-)
-
-
-urlpatterns = [
-    path('', homepage_view, name='home'),
-]
--- CLAMS_web/wrangler/views.py
+++
@@ -1,7 +0,0 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.shortcuts import render, redirect
-from django.http import Http404
-
-
-def homepage_view(request):
-    return render(request, 'home.html')
